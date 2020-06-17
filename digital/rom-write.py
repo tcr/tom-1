0954bbc361261a6e82e203af6caba4021b8f477a
Tim Ryan <trim@fb.com>
2020-06-17 02:30:14 -0400

    NEXT_GEN_WRITE

--- digital/rom-write.py
+++ digital/rom-write.py
@@ -1,3 +1,19 @@
+D_OE = 1
+R_OE = 2
+DR_UnD = 4
+STACK_W_nR = 8
+D_REG_AND_CCK_D = 16
+TOS_BUS_SEL_1 = 32
+TOS_BUS_SEL_2 = 64
+JUMPIF0 = 128
+
+"""
+some comments on pin inputs:
+D_RCK is pulse
+D_CCK is OR(AND(OR(p4, RAM), clk, pulse) and(~clk, pulse))
+R_U/~D is ~D_U/~D
+"""
+
 def start():
   open("rom.hex", "w")
   write('v2.0 raw')
@@ -7,40 +23,53 @@ def write(arg):
   f.write(arg)
   f.write('\n')
 
-def push_literal(arg):
-  write(format(0b00001101, '04x'))
-  write(arg)
+def writeh(code_a, code_b, arg):
+  a = format(code_a, '02x')
+  b = format(code_b, '02x')
+  x = format(arg, '04x')
+  write(a + x[0:2])
+  write(b + x[2:4])
 
-def jump_if_0(dest):
-  write(format(0b10010001, '04x'))
-  write(dest)
+def push_literal(arg):
+  writeh(
+    D_OE | DR_UnD | STACK_W_nR,
+    D_OE | DR_UnD | STACK_W_nR,
+    arg
+  )
+
+def jump_if_0(arg):
+  writeh(
+    D_OE | D_REG_AND_CCK_D | JUMPIF0,
+    D_OE | D_REG_AND_CCK_D | JUMPIF0,
+    arg,
+  )
 
 def nand():
-  write(format(0b01000001, '04x'))
-  write('0000')
+  writeh(
+    D_OE | TOS_BUS_SEL_2,
+    D_OE | TOS_BUS_SEL_2,
+    0x0000
+  )
 
 def store():
-  write(format(0b00100001, '04x'))
-  write('0000')
-
-def load():
-  # write(format(0b00100001, '04x'))
-  write('0000')
+  writeh(
+    D_OE | TOS_BUS_SEL_1,
+    D_OE | TOS_BUS_SEL_1 | TOS_BUS_SEL_2,
+    0x0000
+  )
 
 start()
-push_literal('cafe')
-push_literal('0000')
+push_literal(0xcafe)
+push_literal(0x0000)
 store()
-push_literal('0000')
-load()
-push_literal('0000')
-jump_if_0('0000')
+push_literal(0x0000)
+jump_if_0(0x0000)
 
-
-# push_literal('2244')
-# push_literal('4422')
+# start()
+# push_literal(0x2244)
+# push_literal(0x4422)
 # nand()
-# push_literal('1111')
+# push_literal(0x1111)
 # nand()
-# push_literal('0000')
-# jump_if_0('0000')
+# push_literal(0x0000)
+# jump_if_0(0x0000)
